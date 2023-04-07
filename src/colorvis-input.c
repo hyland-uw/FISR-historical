04440b578525563344aff3ce088084e9aa83037b
hyland-uw <protonk@gmail.com>
2023-04-06 21:59:27 -0700

    Update colorvis-input.c

--- src/colorvis-input.c
+++ src/colorvis-input.c
@@ -78,7 +78,7 @@ int mutate_and_advance(double array[32]) {
 
 typedef struct {
     int iterations_completed;
-    float after_last_iter;
+    float after_first_iter;
 } Q_rsqrt_results;
 
 Q_rsqrt_results Q_rsqrt_iter(float number, uint32_t magic, float tol, int iters) {
@@ -97,7 +97,9 @@ Q_rsqrt_results Q_rsqrt_iter(float number, uint32_t magic, float tol, int iters)
     while (counter < iters) {
         conv.f *= 1.5F - (number * 0.5F * conv.f * conv.f);
         results.iterations_completed = ++counter;
-        results.after_last_iter = conv.f;
+        if (counter == 1) {
+          results.after_first_iter = conv.f;
+        }  
         if (fabsf(ref - conv.f) < tol) {
             break;
         }
@@ -117,19 +119,21 @@ int main() {
     float ref, approx, input;
     int iter_limit = 100;
     int iters = 0;
+    int steps = 0;
 
     create_array32(array);
 
-    printf("input,ref,approx,iters,flipped\n");
+    printf("input,ref,approx,iters,flipped,steps\n");
     while (iters < iter_limit) {
       input = flt_min + ((float)rand() / (float)RAND_MAX) * (flt_max - flt_min);
       Q_rsqrt_results results = Q_rsqrt_iter(input, magic, tol, iter_limit);
+      steps++;
       ref = 1 / sqrtf(input);
-      approx = results.after_last_iter;
+      approx = results.after_first_iter;
       iters = results.iterations_completed;
       flipped = mutate_and_advance(array);
       magic = flip_chosen_bit(magic, flipped);
-      printf("%f,%f,%f,%d,%d\n", input, ref, approx, iters, flipped);
+      printf("%f,%f,%f,%d,%d,%d\n", input, ref, approx, iters, flipped, steps);
     }
     
     return 0;
@@ -137,34 +141,30 @@ int main() {
 
 /* The path of one mutation.
 
-input,ref,approx,iters,flipped
-1.378260,0.851794,0.851793,2,30
-1.586537,0.793916,0.793913,2,31
-0.471355,1.456551,1.456550,2,28
-1.395830,0.846416,0.846415,2,29
-0.620060,1.269940,1.269934,2,26
-1.460584,0.827440,0.827438,2,22
-1.881172,0.729098,0.729097,2,23
-0.306351,1.806717,1.806567,1,24
-1.693480,0.768440,0.768437,2,25
-1.651802,0.778074,0.778071,2,26
-1.813196,0.742639,0.742637,2,19
-1.877477,0.729815,0.729814,2,16
-1.452443,0.829756,0.829753,2,18
-1.010255,0.994911,0.994909,2,20
-1.959349,0.714404,0.713989,1,15
-1.061439,0.970627,0.970627,2,17
-1.470807,0.824560,0.824559,2,13
-1.270229,0.887276,0.887192,1,14
-1.609079,0.788336,0.788335,1,11
-1.320341,0.870276,0.870139,2,12
-1.363609,0.856357,0.856346,2,9
-0.395172,1.590768,1.590765,4,10
-1.096431,0.955013,0.954945,2,6
-0.845823,1.087327,1.087325,10,7
-1.481340,0.821623,0.821583,13,4
-0.556328,1.340708,1.340277,40,8
-1.060998,0.970829,0.970821,39,2
-1.386895,0.849138,inf,100,3
+input,ref,approx,iters,flipped,steps
+1.154656,0.930623,0.930609,1,30,1
+1.995487,0.707906,0.707710,1,31,2
+1.561315,0.800303,0.799048,2,28,3
+1.823150,0.740609,0.739793,2,27,4
+1.561520,0.800251,0.798996,2,26,5
+1.310742,0.873457,0.873056,1,25,6
+1.802190,0.744903,0.744013,2,23,7
+0.485837,1.434678,1.433944,2,24,8
+0.768270,1.140888,1.140149,2,21,9
+0.651364,1.239048,1.236861,2,22,10
+0.586281,1.306012,1.304114,2,19,11
+1.700844,0.766775,0.765576,2,18,12
+0.553746,1.343832,1.342478,2,17,13
+1.718543,0.762816,0.761793,2,13,14
+0.892975,1.058231,1.054437,2,14,15
+1.190156,0.916638,0.915819,2,16,16
+0.992382,1.003831,0.999173,2,15,17
+1.478703,0.822355,0.822346,1,11,18
+0.419416,1.544107,1.528528,2,20,19
+1.514834,0.812489,0.805166,2,12,20
+0.411700,1.558509,1.554541,2,9,21
+1.147631,0.933467,0.761979,4,10,22
+1.328983,0.867442,0.854865,2,8,23
+1.627713,0.783811,-1.757129,100,18,24
 
 */
