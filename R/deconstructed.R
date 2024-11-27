ae7845f925972672a53ed04fb6e114880524ae4f
hyland-uw <protonk@gmail.com>
2024-11-27 11:07:56 -0800

    update combined plot to be colorblind friendly

--- R/deconstructed.R
+++ R/deconstructed.R
@@ -31,9 +31,7 @@ deconstructed <- deconstructed %>%
 
 deconstructed$iter_rank <- cut(as.numeric(deconstructed$iters), 
                          breaks = c(0, 1, 2, 3, 4, 24, 60, 94),
-                         labels = c("1", "2", "3", "4", "5-24", "25-60", "61-94"),
-                         include.lowest = TRUE,
-                         right = FALSE)
+                         labels = c("1", "2", "3", "4", "5-24", "25-60", "61-94"))
 #### wide plot data loading
 widened <- read.csv("../data/deconstructed-wide.csv")
 
@@ -50,9 +48,7 @@ widened <- widened %>%
 
 widened$iter_rank <- cut(as.numeric(widened$iters), 
                          breaks = c(0, 1, 2, 3, 4, 24, 60, 94),
-                         labels = c("1", "2", "3", "4", "5-24", "25-60", "61-94"),
-                         include.lowest = TRUE,
-                         right = FALSE)
+                         labels = c("1", "2", "3", "4", "5-24", "25-60", "61-94"))
 
 mc_annotate <- function(magic_value, label, color, x_start = -0.035, x_end = 0.036, text_size = 8) {
   list(
@@ -95,9 +91,6 @@ c25 <- c(
   "darkturquoise", "green1", "yellow4", "yellow3",
   "darkorange4", "brown"
 )
-
-
-
 
 ### plots here
 
@@ -170,26 +163,35 @@ ggplot(deconstructed,
 
 # plot to be subset
 
+iter_rank_hue <- c("lightblue", colorRampPalette(c("white", "orange1", "red"))(7))
 
-iter_rank_hue <- hue_pal()(9)[c(1, 2, 3, 4, 5, 7, 8, 9)]
-
-deconstructed %>% 
+subset_plot <- deconstructed %>% 
   filter(iters %in% levels(iters)[1:5]) %>%
   ggplot(aes(x = (initial - reference) / reference, y = magic, color = iters)) +
   geom_point(shape = 16, size = 0.65, alpha = 0.95) +
   labs(color = "Iterations\nto converge",
-       title = "Shaded region is 0.049% of the 32 bit integers") +
+       title = "Shaded region is 0.024% of the 32 bit integers") +
   guides(colour = "none") + 
   scale_color_manual(values = iter_rank_hue[1:5]) + 
   scale_x_continuous(breaks = c(0), limits = c(-0.25, 0.25)) +
+  ylim(1.5935e9, 1601175552) + 
   theme(plot.title = element_text(hjust = 0.45),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
         axis.title.y = element_blank(),
         axis.title.x = element_blank()) +
   annotate("rect", alpha=0.2, fill="blue",
-           xmin=-Inf, xmax=Inf,
-           ymin=0x5F300000, ymax=0x5F400000) -> subset_plot
+           xmin=-Inf, xmax=-0.125,
+           ## this is 1048576, 1/8192th of the possible search space
+           ymin=0x5F300000, ymax=0x5F400000) +
+  annotate("rect", alpha=0.2, fill="blue",
+           xmin=0.125, xmax=Inf,
+           ymin=0x5F300000, ymax=0x5F400000) + 
+  geom_hline(yintercept = 0x5F400000,
+             color = "blue", alpha = 0.6, lty = 4, linewidth = 0.2) +
+  geom_hline(yintercept = 0x5F300000,
+             color = "blue", alpha = 0.6, lty = 4, linewidth = 0.2)
+
 ### for this plot, y ranges from 1150202431 to 1601995043
 widened %>% 
   ggplot(aes(x = (initial - reference) / reference,
@@ -199,34 +201,37 @@ widened %>%
              size = 0.65,
              alpha = 0.95,
              show.legend = TRUE) +
-  xlab("Relative error") + ylab("Restoring constant value") + 
+  xlab("Relative error") + ylab("Restoring constant (in billions)") + 
   labs(color = "Iterations\nto converge",
-       title = "Extended to only 21% of the available integer space,\nthe region where the approximation is optimal is invisible.",
+       title = "Extended to only 10% of the available integer space,\nthe region where the approximation is optimal is invisible.",
        fill = "Range of\noptimal integers") +
-  scale_y_continuous(labels = function(x) sprintf("%d", as.integer(x)),
+  scale_y_continuous(labels = function(x) sprintf("%.1f", x / 1e9),
                      limits = c(1.3e9 - 1, NA)) +
   scale_color_manual(values = iter_rank_hue,
                      na.translate = F,
                      drop = FALSE) +
   guides(colour = guide_legend(override.aes = list(size=5))) +
   annotate("rect", alpha=0.2, fill="blue",
-           xmin=-Inf, xmax=Inf,
-           ## this is 1048576, 1/4096th of the possible search space
+           xmin=-Inf, xmax=-0.1,
+           ## this is 1048576, 1/8192th of the possible search space
+           ymin=0x5F300000, ymax=0x5F400000) +
+  annotate("rect", alpha=0.2, fill="blue",
+           xmin=0.1, xmax=Inf,
            ymin=0x5F300000, ymax=0x5F400000) +
   annotate("rect",
            fill = NA, color = "black",
            xmin=-0.5, xmax=0.5,
            ymin=1.325e9, ymax=1.525e9) +
-  annotate("segment", x = -0.2, xend = -0.5, y = 1602500000, yend = 1.525e9, 
+  annotate("segment", x = -0.15, xend = -0.5, y = 1602500000, yend = 1.525e9, 
            linetype = "dashed") +
-  annotate("segment", x = 0.2, xend = 0.5, y = 1602500000, yend = 1.525e9, 
+  annotate("segment", x = 0.15, xend = 0.5, y = 1602500000, yend = 1.525e9, 
            linetype = "dashed") + 
   annotate("rect",color = "black", fill = NA, linetype = "dashed",
-           xmin=-0.2, xmax=0.2,
+           xmin=-0.15, xmax=0.15,
            ymin=1592500000, ymax=1602500000) + 
-  annotate("segment", x = 0.2, xend = 0.5, y = 1592500000, yend = 1.325e9, 
+  annotate("segment", x = 0.15, xend = 0.5, y = 1592500000, yend = 1.325e9, 
            linetype = "dashed") + 
-  annotate("segment", x = -0.2, xend = -0.5, y = 1592500000, yend = 1.325e9, 
+  annotate("segment", x = -0.15, xend = -0.5, y = 1592500000, yend = 1.325e9, 
            linetype = "dashed") + 
   geom_point(aes(fill = "0x5F300000 to\n0x5F400000"), alpha = 0) +
   scale_fill_manual(values = c("0x5F300000 to\n0x5F400000" = "blue"),
